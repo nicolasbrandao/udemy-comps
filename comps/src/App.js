997245fa95d3c50cbdb2e4fbe18b5310343696f5
Nícolas Brandão <107960652+nicolasbrandao@users.noreply.github.com>
2023-01-20 12:07:18 -0300

    Finished with course section 10

---
+++ comps/src/App.js
@@ -0,0 +1,39 @@
+import Button from './Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
+function App () {
+    const handleClick = () => {
+        console.log('Click!');
+    };
+
+    return (
+        <div>
+            <div>
+                <Button className={'mb-5'} primary rounded onClick={handleClick}>
+                    <GoBell />
+                    Click me!
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded>
+                    <GoCloudDownload />
+                    Buy now
+                </Button>
+            </div>
+            <div>
+                <Button success outline rounded>
+                    <GoDatabase />
+                    See deal!
+                </Button>
+            </div>
+            <div>
+                <Button warning>Hide ads!</Button>
+            </div>
+            <div>
+                <Button danger>Log out!!!</Button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
